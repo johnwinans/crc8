2b28d8379a5f007cdb92719c99d21b2076ecc1cd
John Winans <john@winans.org>
2023-04-08 08:57:20 -0500

    Add license blah

--- ttop.v
+++ ttop.v
@@ -1,3 +1,26 @@
+//************************************************************************
+//
+// A parameterizable CRC generator
+//
+// Copyright (C) 2023 John Winans
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// See: https://github.com/johnwinans/crc8
+//
+//************************************************************************
+
 `timescale 1ns/1ps
 
 /**
@@ -11,7 +34,6 @@ module ttop ();
 	reg data_ref;			///< reflected input bit-stream
 	reg enable;				///< advance the CRC calc on the next clk
 	reg ready;				///< true when the CRC value is valid
-
 
 	// little endian = [n:0]
 	// big endian = [0:n]
