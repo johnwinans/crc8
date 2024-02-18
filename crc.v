6157e4d50ec63d1df7ca74f80fce8697ff69a632
John Winans <john@winans.org>
2024-02-18 12:24:55 -0600

    whitespace

--- crc.v
+++ crc.v
@@ -28,39 +28,39 @@
 ***************************************************************************/
 module crc 
 #(
-	// default settings for crc8_wcdma
-	parameter	BITS	= 8,			///< How many bits wide is the CRC
-	parameter	POLY	= 8'h9B,		///< The CRC polynomial
-	parameter	INIT	= 8'h00,		///< the initial value of the CRC
-	parameter	XOR_OUT = 8'h00,		///< XOR the result
-	parameter	REF_OUT	= 1				///< reverse the bit order of the CRC
+    // default settings for crc8_wcdma
+    parameter   BITS    = 8,            ///< How many bits wide is the CRC
+    parameter   POLY    = 8'h9B,        ///< The CRC polynomial
+    parameter   INIT    = 8'h00,        ///< the initial value of the CRC
+    parameter   XOR_OUT = 8'h00,        ///< XOR the result
+    parameter   REF_OUT = 1             ///< reverse the bit order of the CRC
 )
 (
-	input wire clk,						///< accept a new bit on rising edge
-	input wire rst,						///< sync reset when true and clk rising edge
-	input wire data,					///< message data bits
-	input wire enable,					///< accept data when high & clk rising
-	output wire [7:0] crc_out			///< the running value of calculated crc
+    input wire clk,                     ///< accept a new bit on rising edge
+    input wire rst,                     ///< sync reset when true and clk rising edge
+    input wire data,                    ///< message data bits
+    input wire enable,                  ///< accept data when high & clk rising
+    output wire [7:0] crc_out           ///< the running value of calculated crc
 );
 
-	reg [BITS-1:0] crc_reg;
+    reg [BITS-1:0] crc_reg;
 
-	wire xdi = crc_reg[BITS-1]^data;
-	wire [BITS-1:0] poly_reg = {POLY[BITS-1:1],1'b0};	// turn off the low bit
+    wire xdi = crc_reg[BITS-1]^data;
+    wire [BITS-1:0] poly_reg = {POLY[BITS-1:1],1'b0};   // turn off the low bit
 
-	always @(posedge clk) begin
-		if (rst) begin
-			crc_reg <= INIT;
-		end else if (enable) begin
-			crc_reg <= {crc_reg[6:0], xdi} ^ (xdi ? poly_reg : 0);
-		end
-	end
+    always @(posedge clk) begin
+        if (rst) begin
+            crc_reg <= INIT;
+        end else if (enable) begin
+            crc_reg <= {crc_reg[6:0], xdi} ^ (xdi ? poly_reg : 0);
+        end
+    end
 
-	// reflect the output value & XOR the result... or not
-	genvar j;
-	generate for(j=0; j<BITS; j=j+1) 
-		assign crc_out[j] = (REF_OUT ? crc_reg[BITS-1-j] : crc_reg[j]) ^ XOR_OUT[j]; 
-	endgenerate
+    // reflect the output value & XOR the result... or not
+    genvar j;
+    generate for(j=0; j<BITS; j=j+1) 
+        assign crc_out[j] = (REF_OUT ? crc_reg[BITS-1-j] : crc_reg[j]) ^ XOR_OUT[j]; 
+    endgenerate
 
 endmodule
 
